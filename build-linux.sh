7f08d590a7f87781332844fd965ceacd17a494b8
Leopard-C <leopard.c@outlook.com>
2023-12-11 00:45:04 +0800

    update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -4,13 +4,15 @@ set -e
 
 if [ ! -d build ]; then mkdir build; fi
 if [ ! -d lib/linux ]; then mkdir -p lib/linux; fi
+if [ ! -d include/md4c ]; then mkdir -p include/md4c; fi
+
 cd build
 cmake ..
 make -j
 cp src/*.a ../lib/linux
 
 cd ..
-echo '-----------------------------'
-echo 'ls -l lib/linux'
-ls -l lib/linux
+cp src/entity.h src/md4c-html.h src/md4c.h include/md4c
+
+echo 'Success!'
 
